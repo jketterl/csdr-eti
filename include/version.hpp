bba1c5023dd8ea8fa74f2293f5aae4be5cc57d83
Jakob Ketterl <jakob.ketterl@gmx.de>
2024-01-22 02:18:08 +0100

    add version information

---
+++ include/version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace Csdr::Eti {
+
+    extern const std::string version;
+
+}
---
+++ src/version.cpp
@@ -0,0 +1,7 @@
+#include "version.hpp"
+
+namespace Csdr::Eti {
+
+    const std::string version = VERSION;
+
+}
